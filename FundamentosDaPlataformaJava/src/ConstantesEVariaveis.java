76482588d772c25e6cd787a70711e64963478567
duduzx02 <duduzx02@gmail.com>
2023-11-07 00:12:12 -0300

    Variáveis e Métodos

---
+++ FundamentosDaPlataformaJava/src/ConstantesEVariaveis.java
@@ -0,0 +1,17 @@
+public class ConstantesEVariaveis {
+    public static void main(String[] args) {
+
+        final String BR = "Brasil";
+        final double PI = 3.14;
+        final int ESTADOS_BRASILEIROS = 27;
+        final int ANO_2000 = 2000;
+
+
+        int numero_um = 1;
+        int numero1 = 1;
+        int numeroUm = 1;
+        int longo = 1;
+
+
+    }
+}
