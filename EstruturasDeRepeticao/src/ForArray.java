18e2ac83183c69164e72f9192085674ea8d6ccf1
duduzx02 <duduzx02@gmail.com>
2023-11-09 01:27:27 -0300

    For Each

---
+++ EstruturasDeRepeticao/src/ForArray.java
@@ -0,0 +1,10 @@
+public class ForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+        for (String aluno:
+             alunos) {
+            System.out.println(aluno);
+        }
+    }
+}
