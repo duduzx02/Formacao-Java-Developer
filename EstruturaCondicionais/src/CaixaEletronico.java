ea339fb7e19a848c527beaa5d96176325140f7f2
duduzx02 <duduzx02@gmail.com>
2023-11-08 23:27:07 -0300

    Condicional simples

---
+++ EstruturaCondicionais/src/CaixaEletronico.java
@@ -0,0 +1,12 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 45000.00;
+        double saque = 150000.00;
+
+        if(saldo > saque){
+            saldo = saldo - saque;
+        }
+
+        System.out.println(saldo);
+    }
+}
