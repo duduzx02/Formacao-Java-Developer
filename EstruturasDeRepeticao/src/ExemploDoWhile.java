cb4d1c99e62f35dc0d8a40cfa68f6fe1b261a055
duduzx02 <duduzx02@gmail.com>
2023-11-09 02:15:41 -0300

    Do While

---
+++ EstruturasDeRepeticao/src/ExemploDoWhile.java
@@ -0,0 +1,10 @@
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do{
+            System.out.println("Tocando...");
+            i++;
+        } while(i < 12);
+    }
+}
