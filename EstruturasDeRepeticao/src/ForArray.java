17764876b72f8e663f9807173ce8c5ba8219dd8d
duduzx02 <duduzx02@gmail.com>
2023-11-09 01:31:48 -0300

    For em Arrays

--- EstruturasDeRepeticao/src/ForArray.java
+++ EstruturasDeRepeticao/src/ForArray.java
@@ -2,9 +2,8 @@ public class ForArray {
     public static void main(String[] args) {
         String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
 
-        for (String aluno:
-             alunos) {
-            System.out.println(aluno);
+        for (int x = 0; x < alunos.length; x++){
+            System.out.printf("Aluno na posição %d é %s%n", x, alunos[x]);
         }
     }
 }
