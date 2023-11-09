a3119e4840cfcf0069cdf3c13984c9e1bee9b264
duduzx02 <duduzx02@gmail.com>
2023-11-08 23:41:43 -0300

    Condição ternária

--- EstruturaCondicionais/src/ResultadoEscolar.java
+++ EstruturaCondicionais/src/ResultadoEscolar.java
@@ -1,14 +1,10 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-        double nota = 7.0;
+        double nota = 4.9;
 
-        if(nota >= 7.0){
-            System.out.println("Aprovado!");
-        }else if(nota <=  7.0 && nota >= 5.0 ){
-            System.out.println("Recuperação!!");
-        } else {
-            System.out.println("Reprovado!");
-        }
+        String resultado = nota >= 7.0 ? "Aprovado!" : nota >= 5.0 ? "Recuperação!!" : "Reprovado!!";
+
+        System.out.println(resultado);
     }
 }
