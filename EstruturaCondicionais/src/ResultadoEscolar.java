2dc61fd42304113f0c4ba9858f7f0744d788b683
duduzx02 <duduzx02@gmail.com>
2023-11-08 23:37:55 -0300

    Condicional encadeada

--- EstruturaCondicionais/src/ResultadoEscolar.java
+++ EstruturaCondicionais/src/ResultadoEscolar.java
@@ -1,11 +1,13 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-        double nota = 6.9;
+        double nota = 7.0;
 
         if(nota >= 7.0){
             System.out.println("Aprovado!");
-        }else{
+        }else if(nota <=  7.0 && nota >= 5.0 ){
+            System.out.println("Recuperação!!");
+        } else {
             System.out.println("Reprovado!");
         }
     }
