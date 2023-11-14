020fd2a1f1896defe949d0420c09b93f6eb620a6
duduzx02 <duduzx02@gmail.com>
2023-11-14 15:09:35 -0300

    Fazendo ligações para cada candidato selecionado

--- ProcessoSeletivo/src/Candidatura/ProcessoSeletivo.java
+++ ProcessoSeletivo/src/Candidatura/ProcessoSeletivo.java
@@ -1,12 +1,46 @@
 package Candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
   //      selecaoCandidatos();
-        imprimirSelecionados();
+  //      imprimirSelecionados();
+
+        String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+        for (String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
     }
+
+        static void entrandoEmContato(String candidato){
+            int tentativasRealizadas = 1;
+            boolean continuarTentando = true;
+            boolean atendeu = false;
+
+           do{
+               atendeu = atender();
+               continuarTentando = !atendeu;
+               if(continuarTentando){
+                   tentativasRealizadas++;
+               } else {
+                   System.out.println("Contato realizado com sucesso!");
+               }
+           } while(continuarTentando && tentativasRealizadas<3);
+
+           if(atendeu){
+               System.out.println("Conseguimos contanto com " + candidato +" na " + tentativasRealizadas);
+           } else {
+               System.out.println("Não conseguimos contato com " + candidato);
+           }
+        }
+
+    // método auxiliar
+        static boolean atender(){
+            return new Random().nextInt(3)==1;
+        }
 
     static void imprimirSelecionados(){
         String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
