a76cf778f758de575d5da61f45d48d3e61bdd484
Eduardo Lima <duduzx02@gmail.com>
2023-12-05 09:15:46 -0300

    Reprodutor Musical do iPhone

---
+++ FundamentosDaProgramacaoOrientadaaObjetosComJava/iPhone/src/ReprodutorMusical/Musica.java
@@ -0,0 +1,15 @@
+package ReprodutorMusical;
+
+public class Musica {
+    String titulo;
+    String artista;
+    String album;
+    int duracao;
+
+    public Musica(String titulo, String artista, String album, int duracao) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.album = album;
+        this.duracao = duracao;
+    }
+}
