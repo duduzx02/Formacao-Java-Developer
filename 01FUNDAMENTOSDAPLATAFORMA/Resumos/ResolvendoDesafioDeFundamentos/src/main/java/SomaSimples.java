4b6fb3b7a1a9956cce7036d1980f991f2182d6de
Eduardo Lima <duduzx02@gmail.com>
2024-06-18 03:23:35 -0300

    Resumo: Soma simples

---
+++ 01FUNDAMENTOSDAPLATAFORMA/Resumos/ResolvendoDesafioDeFundamentos/src/main/java/SomaSimples.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SomaSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int soma = a + b;
+        System.out.println("SOMA = " + soma);
+    }
+}
