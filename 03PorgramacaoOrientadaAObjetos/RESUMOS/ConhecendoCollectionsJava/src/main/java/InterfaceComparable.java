58fb7d2aa678b944eae384525fb9febf5d972ee6
Eduardo Lima <duduzx02@gmail.com>
2024-07-06 00:43:29 -0300

    Resumo: Interface Comparator

--- 03PorgramacaoOrientadaAObjetos/RESUMOS/ConhecendoCollectionsJava/src/main/java/InterfaceComparable.java
+++ 03PorgramacaoOrientadaAObjetos/RESUMOS/ConhecendoCollectionsJava/src/main/java/InterfaceComparable.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class InterfaceComparable {
 
     static class Pessoas implements Comparable<Pessoas> {
         private String nome;
+        private int idade;
 
-        public Pessoas(String nome) {
+        public int getIdade() {
+            return idade;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        public Pessoas(String nome, int idade) {
             this.nome = nome;
+            this.idade = idade;
         }
 
         @Override
@@ -16,13 +27,21 @@ public class InterfaceComparable {
             return this.nome.compareTo(o.nome);
         }
 
+        static class IdadeComparator implements Comparator<Pessoas> {
+
+            @Override
+            public int compare(Pessoas o1, Pessoas o2) {
+                return Integer.compare(o1.getIdade(), o2.getIdade());
+            }
+        }
+
         public static void main(String[] args) {
             List<Pessoas> pessoas = new ArrayList<>();
-            pessoas.add(new Pessoas("Maria"));
-            pessoas.add(new Pessoas("Joaquim"));
-            pessoas.add(new Pessoas("Pedro"));
-            pessoas.add(new Pessoas("Ana"));
-            pessoas.add(new Pessoas("Carlos"));
+            pessoas.add(new Pessoas("Maria", 25));
+            pessoas.add(new Pessoas("Joaquim", 30));
+            pessoas.add(new Pessoas("Pedro", 20));
+            pessoas.add(new Pessoas("Ana", 40));
+            pessoas.add(new Pessoas("Carlos", 35));
 
             // foreach
             for(Pessoas p : pessoas) {
@@ -35,6 +54,15 @@ public class InterfaceComparable {
             Collections.sort(pessoas);
             for(Pessoas p : pessoas) {
                 System.out.println(p.nome);
+            }
+
+
+
+            // Comparator
+            System.out.println("Depois do Comparator comparando por idade: ");
+            Collections.sort(pessoas, new IdadeComparator());
+            for(Pessoas p : pessoas) {
+                System.out.println(p.nome + " " + p.getIdade());
             }
         }
     }
